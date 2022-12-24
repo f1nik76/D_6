255e8577236b6e247cf22769852d56ca07b45fb5
f1nik <f1nik.yar@gmail.com>
2022-12-24 09:18:05 +0300

    D 7

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from news.views import ListNews, NewsDetail
+from news.views import ListNews, NewsDetail, SearchListView, NewsCreate, NewsUpdate, NewsDelete
 
 urlpatterns = [
-    path('news/', ListNews.as_view()),
-    path('news/<pk>', NewsDetail.as_view()),
+    path('news/', ListNews.as_view(), name='news'),
+    path('search/', SearchListView.as_view(), name='news_search'),
+    path('news/<pk>', NewsDetail.as_view(), name='news_detail'),
+    path('create/', NewsCreate.as_view(), name='news_create'),
+    path('news/<int:pk>/update/', NewsUpdate.as_view(), name='news_update'),
+    path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .filters import NewsFilter
 from news.models import News
+from .forms import NewsForm
 
 
 # Create your views here.
@@ -12,9 +14,58 @@ class ListNews(ListView):
     ordering = 'date_pub'
     template_name = 'news/news.html'
     context_object_name = 'news'
+    paginate_by = 2
+
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
 
 
 class NewsDetail(DetailView):
     model = News
     template_name = 'news/news_detail.html'
     context_object_name = 'news'
+
+
+
+
+class SearchListView(ListNews):
+    template_name = 'news/search.html'
+
+# Добавляем новое представление для создания товаров.
+class NewsCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = NewsForm
+    # модель товаров
+    model = News
+    # и новый шаблон, в котором используется форма.
+    template_name = 'news/news_edit.html'
+
+class NewsUpdate(UpdateView):
+    # Указываем нашу разработанную форму
+    form_class = NewsForm
+    # модель товаров
+    model = News
+    # и новый шаблон, в котором используется форма.
+    template_name = 'news/news_edit.html'
+
+class NewsDelete(DeleteView):
+    model = News
+    context_object_name = 'news'
+    template_name = 'news/news_delete.html'
+    success_url = reverse_lazy('news')
